56caa5fcdc41a945d39a77a6f5b3cb5fc8dbc44e
Michael Nascimento <michael.nsc@outlook.com>
2022-07-28 22:11:29 -0300

    Resolução em aula

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
-alert("Funcionou!");
+const renderizaCard = (nome, stack) => {
+  return /*html*/ `
+  <div class="card">
+    <span class="card__titulo">${nome}</span>
+    <span class="card__stack">${stack}</span>
+  </div>
+  `;
+};
+
+const renderizaTela = (listaDeAlunos = []) => {
+  const listaCardsEl = document.getElementById("lista-cards");
+  listaCardsEl.innerHTML = null;
+  listaDeAlunos.forEach((aluno) => {
+    listaCardsEl.innerHTML += renderizaCard(aluno.nome, aluno.stack);
+  });
+};
+
+fetch("assets/database/dados.json")
+  .then((resultado) => resultado.json())
+  .then((dados) => {
+    const alunos = dados.alunos;
+    renderizaTela(alunos);
+  });
+
+// // SWAPI
+// // Remova o comentário desse código para ver os dados do Luke
+// // no console do navegador!!
+// fetch("https://swapi.dev/api/people/1")
+// .then((res) => res.json())
+// .then((data) => console.log(data));
